8582b5a3831644300ada918f8897f8651a92192d
VanKLuu <luukhanhvan@gmail.com>
2021-11-04 19:52:17 -0700

    no message

--- Assignment9/optionTwo.h
+++ Assignment9/optionTwo.h
@@ -25,11 +25,9 @@ int guess(int low, int high, int &guessCount)
 	{
 		guessCount++;
 		midPoint = (low + high) / 2;
-		//cout << "\n\tIs your number " << midPoint << "? (Y-yes or N-no) ";
 		choice = inputChar("\n\tIs your number " + to_string(midPoint) + "? (Y-yes or N-no) ", 'Y', 'N');
 		while (toupper(choice) == 'N')
 		{
-			//cout << "\n\tIs your number larger than " << midPoint << "? (Y-yes or N-no) ";
 			choice = inputChar("\n\tIs your number larger than " + to_string(midPoint) + "? (Y-yes or N-no) ", 'Y', 'N');
 			if (toupper(choice) == 'Y')
 				return guess(midPoint + 1, high, guessCount);
